d05b2fd0029f9610cf31b682492870ce6abc7f4c
Maharshi Mehta <maharshinmehta@gmail.com>
2026-04-20 18:03:17 +0530

    functions end

---
+++ Day 1/Functions/Expense.py
@@ -0,0 +1,10 @@
+def add_income(balance,amount):
+    return balance + amount
+
+def add_expense(balance,amount):
+    return balance - amount
+
+print(add_income(1500,3000))#4500
+print(add_expense(1500,500))#1000
+
+
--- Day 1/Functions/functions.py
+++ Day 1/Functions/functions.py
@@ -24,3 +24,52 @@ def add(a,b):
 
 result = add(12,3)
 print(result)#15
+
+
+# Default Parameters
+def greet(name="User"):
+    print("Hello",name)
+
+greet()
+greet("Maharshi")
+
+#Multiple Returns (Advanced Thinking)
+def calc(a,b):
+    return a+b,a-b #Hello Maharshi
+
+sum_val,diff_val = calc(10,2)
+print(sum_val,diff_val)# 12 8
+
+#Function Inside Loop
+def process_transaction(t):
+    return t*2
+
+transaction=[10,20,30]
+
+for t in transaction:
+    print(process_transaction(t))
+# 👉 Used in:
+
+# Data processing
+# Pipelines
+# APIs
+
+# ⚠️ Common Mistakes
+
+# ❌ Not using functions → messy code
+# ❌ Using print instead of return
+# ❌ Writing huge functions (bad practice)
+
+
+# 🧠 Industry Rule
+
+# 👉 One function = one responsibility
+
+#Bad Approach ❌
+# def everything():
+#     # login + payment + print + save 
+
+#Better Approach
+def login(): pass
+def process_payment(): pass
+def save_data(): pass
